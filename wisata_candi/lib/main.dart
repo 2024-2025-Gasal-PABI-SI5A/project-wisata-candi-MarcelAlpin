8a216bc70dc796d55e6a09ee31495d84294efcdd
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2024-11-15 09:42:41 +0700

    Update main.dart

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/signin_screen.dart';
 
@@ -22,8 +22,8 @@ class MainApp extends StatelessWidget {
             surface: Colors.deepPurple[50],
           ),
           useMaterial3: true),
-      //home:DetailScreen(candi: CandiData.candiList[0]),
-      //home:ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      // home: ProfileScreen(),
       home: SignInScreen(),
     );
   }
